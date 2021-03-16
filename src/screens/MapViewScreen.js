69c44c0971fe837197a34229e483fb96eba87702
Nguyễn Thị Thúy <thuynguyen03091999@gmail.com>
2021-03-16 16:43:49 +0700

    Show map view

--- src/screens/MapViewScreen.js
+++ src/screens/MapViewScreen.js
@@ -1,35 +1,56 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {View, StyleSheet, Text} from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 
-export default function MapViewScreen() {
-  return (
-    <View style={styles.container}>
-      <Text> He he he</Text>
-      <MapView
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
-    </View>
-  );
+export default class MapViewScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+    };
+  }
+  getInitialState() {
+    return {
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+    };
+  }
+
+  onRegionChange = region => {
+    this.setState({region});
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+          style={styles.map}
+          region={this.state.region}
+          onRegionChange={this.onRegionChange}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    ...StyleSheet.absoluteFillObject,
+    height: '100%',
+    width: '100%',
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#FFF6F9',
   },
-  title: {
-    color: '#000000',
-  },
-  image: {
-    height: 200,
-    width: 200,
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
