478c17d2832b815cb8a3e9dc31d2526a0df75c04
Nguyễn Thị Thúy <thuynguyen03091999@gmail.com>
2021-04-02 13:27:11 +0700

    Update

--- src/screens/MapViewScreen.js
+++ src/screens/MapViewScreen.js
@@ -63,9 +63,7 @@ class MapViewScreen extends React.Component {
     //     .catch(error => console.warn(error));
     this.getCurrentLocation();
   }
-  getCurrentLocation = () => {
-    getLocation();
-  };
+  getCurrentLocation = () => {};
   onChangeText = text => {
     this.setState({startLocation: text});
   };
