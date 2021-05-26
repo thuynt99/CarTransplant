bd8caf34d8d6bec7e1f4236d2fdc62810dad28b3
thuynt99 <thuynguyen03091999@gmail.com>
2021-05-27 01:00:00 +0700

    Add reservation item

--- src/components/MyReservation/MyReservationUpcomming/ListMyReservationUpComing.js
+++ src/components/MyReservation/MyReservationUpcomming/ListMyReservationUpComing.js
@@ -20,6 +20,7 @@ import {Image} from 'react-native';
 import {ScaledSheet} from 'react-native-size-matters';
 import theme from '../../../theme';
 import {Rating} from 'react-native-ratings';
+import ItemReservation from '../ItemReservation';
 
 class ListMyReservationUpComing extends Component {
   render() {
@@ -28,85 +29,7 @@ class ListMyReservationUpComing extends Component {
         <Content>
           <List>
             <ListItem noBorder>
-              <Card style={styles.card}>
-                <Item style={styles.top}>
-                  <Left>
-                    <Text>Ngày 15/05/2021</Text>
-                  </Left>
-                  <Right>
-                    <Text>11:40 đến 12:10</Text>
-                  </Right>
-                </Item>
-                <Item style={styles.center}>
-                  <Left>
-                    <View>
-                      <View style={styles.location}>
-                        <Icon
-                          name="person-pin-circle"
-                          type="MaterialIcons"
-                          style={{color: 'green', fontSize: 28}}
-                        />
-                        <Text style={styles.subTitle}>Điểm đón:</Text>
-                      </View>
-                      <Text style={{paddingLeft: 30}}>
-                        1096 Đường Láng, Yên Hòa, Đống Đa, Hà Nội
-                      </Text>
-                    </View>
-                    <View>
-                      <View style={styles.location}>
-                        <Icon
-                          name="location-on"
-                          type="MaterialIcons"
-                          style={{color: theme.primaryColor}}
-                        />
-                        <Text style={styles.subTitle}>Điểm đến:</Text>
-                      </View>
-                      <Text style={{paddingLeft: 30}}>
-                        Thái Hòa, Lập Thạch, Vĩnh Phúc
-                      </Text>
-                    </View>
-                  </Left>
-                </Item>
-                <Item style={styles.vehicleTypeView}>
-                  <Left>
-                    <Text>Mã chuyến đi: #12345</Text>
-                  </Left>
-                  <Right>
-                    <Text>Ghép người</Text>
-                  </Right>
-                </Item>
-                <Row style={styles.bottom}>
-                  <Left>
-                    <Row>
-                      <Image
-                        style={{
-                          width: 50,
-                          height: 50,
-                        }}
-                        source={{
-                          uri: 'https://reactnative.dev/img/tiny_logo.png',
-                        }}
-                      />
-                      <View style={styles.viewDriver}>
-                        <Text>John Smith</Text>
-                        <Rating ratingCount={5} imageSize={18} />
-                      </View>
-                    </Row>
-                  </Left>
-                  <Right>
-                    <Row>
-                      <View>
-                        <Text style={styles.subTitle}>Thời gian</Text>
-                        <Text style={styles.value}>1.3h</Text>
-                      </View>
-                      <View>
-                        <Text style={styles.subTitle}>Giá tiền</Text>
-                        <Text style={styles.value}>300.000</Text>
-                      </View>
-                    </Row>
-                  </Right>
-                </Row>
-              </Card>
+              <ItemReservation />
             </ListItem>
           </List>
         </Content>
@@ -161,6 +84,24 @@ const styles = ScaledSheet.create({
   },
   state: {
     fontWeight: 'bold',
+  },
+  name: {
+    fontSize: '14@ms',
+    fontWeight: 'bold',
+  },
+  textLocation: {
+    paddingLeft: '30@s',
+    fontSize: '16@ms',
+  },
+  vehicleInfo: {
+    borderRadius: 5,
+    backgroundColor: theme.grey_dark,
+    paddingVertical: '3@vs',
+    paddingHorizontal: '5@s',
+    marginTop: '3@vs',
+  },
+  textVehicleInfo: {
+    fontSize: '14@ms',
   },
 });
 export default ListMyReservationUpComing;
