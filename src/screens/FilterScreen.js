2518c0f7d12f4673f5a46c8069989bb205c4f15f
thuynt99 <thuynguyen03091999@gmail.com>
2021-06-10 21:13:51 +0700

    update ui

--- src/screens/FilterScreen.js
+++ src/screens/FilterScreen.js
@@ -4,21 +4,18 @@ import {
   Card,
   CardItem,
   Container,
+  Content,
   Icon,
-  Item,
-  Label,
-  Left,
-  Right,
-  Row,
-  Spinner,
-  Tab,
-  Tabs,
   View,
 } from 'native-base';
 import CheckBox from '@react-native-community/checkbox';
 import React, {Component} from 'react';
 import {ScaledSheet} from 'react-native-size-matters';
-import {FlatList, TouchableOpacity} from 'react-native-gesture-handler';
+import {
+  FlatList,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native-gesture-handler';
 import ItemBookingCar from '../components/MapView/ListCar/ItemBookingCar';
 import ImageIcon from '../components/common/ImageIcon';
 import {
@@ -38,15 +35,36 @@ import _ from 'lodash';
 import {getListActiveZone, postListActiveZone} from '../stores/trip/actions';
 import ModalDropdown from 'react-native-modal-dropdown';
 import {Calendar} from 'react-native-calendars';
+import moment from 'moment';
+import {FORMAT} from '../constants/format';
 
+const listCheckBox = [
+  {
+    id: 1,
+    name: 'Đi riêng',
+  },
+  {
+    id: 2,
+    name: 'Đi ghép',
+  },
+  {
+    id: 3,
+    name: 'Chở hàng',
+  },
+];
 class FilterScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
       loading: false,
       open: false,
-      value: null,
       items: DEPARTMENT,
+      date: {},
+      seat: 0,
+      _markedDates: {},
+      selectedCarId: [],
+      from: 222,
+      to: 222,
     };
     this.setValue = this.setValue.bind(this);
   }
@@ -56,84 +74,189 @@ class FilterScreen extends Component {
     });
   }
 
-  setValue = (idx, value) => {
+  setValue = (idx, value, key) => {
     console.log(value);
-    this.setState({value: value});
+    this.setState({[key]: value});
   };
+  onDaySelect = day => {
+    this.setState({date: day});
+    const _selectedDay = moment(day.dateString).format(FORMAT.YEAR_DATE);
 
+    let marked = true;
+    if (this.state._markedDates[_selectedDay]) {
+      marked = !this.state._markedDates[_selectedDay].marked;
+    }
+
+    // Create a new object using object property spread since it should be immutable
+    // Reading: https://davidwalsh.name/merge-objects
+    const updatedMarkedDates = {
+      ...{
+        [_selectedDay]: {
+          selected: marked,
+          startingDay: true,
+          color: theme.primaryColor,
+          endingDay: true,
+        },
+      },
+    };
+    this.setState({_markedDates: updatedMarkedDates});
+  };
+  onCheckBoxPress = id => {
+    let tmp = this.state.selectedCarId;
+
+    if (tmp.includes(id)) {
+      tmp.splice(tmp.indexOf(id), 1);
+    } else {
+      tmp.push(id);
+    }
+    this.setState({
+      selectedCarId: tmp,
+    });
+  };
   componentDidMount() {}
   render() {
+    const {date, seat} = this.state;
+    const {hideFilter} = this.props;
     const data = _.map(DEPARTMENT, item => item.name);
     return (
       <Container style={styles.container}>
-        <Card style={styles.inLine}>
-          <Icon
-            active
-            name="close"
-            type="EvilIcons"
-            style={{fontSize: 24, color: theme.black}}
-          />
-          <TouchableOpacity>
-            <Text>Bỏ lọc</Text>
-          </TouchableOpacity>
-        </Card>
-        <Card style={styles.card}>
-          <Text>Tỉnh thành</Text>
-          <Text>Chọn tỉnh thành theo vị trí</Text>
-          <View style={styles.inLine}>
-            <ModalDropdown
-              style={styles.dropdown_6}
-              options={data}
-              defaultValue="Tỉnh thành"
-              dropdownStyle={styles.dropdown_2_dropdown}
-              onSelect={(idx, value) => this.setValue(idx, value)}
-            />
-            <Image
-              style={styles.icon}
-              source={{
-                uri:
-                  'https://img.icons8.com/plasticine/100/000000/double-right.png',
-              }}
-            />
-            <ModalDropdown
-              style={styles.dropdown_6}
-              options={data}
-              defaultValue="Tỉnh thành"
-              dropdownStyle={styles.dropdown_2_dropdown}
-              onSelect={(idx, value) => this.setValue(idx, value)}
-            />
-          </View>
-          <Calendar
-            current={'2012-03-01'}
-            minDate={'2012-05-10'}
-            maxDate={'2012-05-30'}
-            onDayPress={day => {
-              console.log('selected day', day);
-            }}
-            onDayLongPress={day => {
-              console.log('selected day', day);
-            }}
-            monthFormat={'yyyy MM'}
-            onMonthChange={month => {
-              console.log('month changed', month);
-            }}
-            hideArrows={true}
-            hideExtraDays={true}
-            disableMonthChange={true}
-            firstDay={1}
-            hideDayNames={true}
-            showWeekNumbers={true}
-            onPressArrowLeft={subtractMonth => subtractMonth()}
-            onPressArrowRight={addMonth => addMonth()}
-            disableArrowLeft={true}
-            disableArrowRight={true}
-            disableAllTouchEventsForDisabledDays={true}
-            renderHeader={date => {
-              /*Return JSX*/
-            }}
-            enableSwipeMonths={true}
-          />
-        </Card>
+        <Content>
+          <Card>
+            <CardItem style={styles.inLine}>
+              <TouchableOpacity onPress={hideFilter}>
+                <Icon
+                  active
+                  name="close"
+                  type="EvilIcons"
+                  style={{fontSize: 24, color: theme.black}}
+                />
+              </TouchableOpacity>
+
+              <TouchableOpacity>
+                <Text>Bỏ lọc</Text>
+              </TouchableOpacity>
+            </CardItem>
+          </Card>
+          <ScrollView style={styles.scroll}>
+            <Card style={styles.card}>
+              <Text style={styles.textTitleCar}>Tỉnh thành</Text>
+              <Text>Chọn tỉnh và tỉnh đến để tìm khách theo vị trí</Text>
+              <View style={styles.inLine}>
+                <ModalDropdown
+                  style={styles.dropdown_6}
+                  options={data}
+                  defaultValue="Tỉnh thành"
+                  dropdownStyle={styles.dropdown_2_dropdown}
+                  onSelect={(idx, value) => this.setValue(idx, value, 'from')}
+                />
+                <Image
+                  style={styles.icon}
+                  source={{
+                    uri:
+                      'https://img.icons8.com/plasticine/100/000000/double-right.png',
+                  }}
+                />
+                <ModalDropdown
+                  style={styles.dropdown_6}
+                  options={data}
+                  defaultValue="Tỉnh thành"
+                  dropdownStyle={styles.dropdown_2_dropdown}
+                  onSelect={(idx, value) => this.setValue(idx, value, 'to')}
+                />
+              </View>
+            </Card>
+            <Card style={styles.card}>
+              <Text style={styles.textTitleCar}>Loại hình</Text>
+              <CardItem style={styles.cardItem}>
+                {listCheckBox.map(item => {
+                  return (
+                    <View style={styles.row}>
+                      <CheckBox
+                        value={
+                          this.state.selectedCarId.includes(item.id)
+                            ? true
+                            : false
+                        }
+                        onChange={() => this.onCheckBoxPress(item.id)}
+                      />
+                      <Text>{item?.name}</Text>
+                    </View>
+                  );
+                })}
+              </CardItem>
+            </Card>
+            <Card style={styles.card}>
+              <CardItem style={styles.cardItem}>
+                <Text style={styles.textTitleCar}>Số người</Text>
+                <View style={styles.row}>
+                  {seat > 0 && (
+                    <TouchableOpacity
+                      style={styles.btnPlus}
+                      onPress={() =>
+                        this.setState({
+                          seat: seat - 1,
+                        })
+                      }>
+                      <Icon
+                        name="minus"
+                        type="Entypo"
+                        style={{color: theme.primaryColor}}
+                      />
+                    </TouchableOpacity>
+                  )}
+                  <Text style={styles.textSeat}>{seat}</Text>
+                  <TouchableOpacity
+                    style={styles.btnPlus}
+                    onPress={() =>
+                      this.setState({
+                        seat: seat + 1,
+                      })
+                    }>
+                    <Icon
+                      name="plus"
+                      type="Entypo"
+                      style={{color: theme.primaryColor}}
+                    />
+                  </TouchableOpacity>
+                </View>
+              </CardItem>
+            </Card>
+            <Card style={styles.card}>
+              <Calendar
+                current={moment().format(FORMAT.YEAR_DATE)}
+                minDate={moment().format(FORMAT.YEAR_DATE)}
+                onDayPress={day => {
+                  this.onDaySelect(day);
+                }}
+                theme={{
+                  calendarBackground: theme.white,
+                  textSectionTitleColor: theme.black,
+                  textSectionTitleDisabledColor: 'gray',
+                  dayTextColor: theme.black,
+                  todayTextColor: theme.thirdColor,
+                  selectedDayTextColor: theme.white,
+                  monthTextColor: theme.thirdColor,
+                  indicatorColor: theme.thirdColor,
+                  selectedDayBackgroundColor: '#333248',
+                  arrowColor: theme.thirdColor,
+                  'stylesheet.calendar.header': {
+                    week: {
+                      marginTop: 30,
+                      marginHorizontal: 12,
+                      flexDirection: 'row',
+                      justifyContent: 'space-between',
+                    },
+                  },
+                }}
+                markingType={'period'}
+                markedDates={this.state._markedDates}
+              />
+            </Card>
+            <Button full danger style={styles.btn}>
+              <Text style={styles.text}>Áp dụng</Text>
+            </Button>
+          </ScrollView>
+        </Content>
       </Container>
     );
   }
@@ -143,9 +266,13 @@ const styles = ScaledSheet.create({
   container: {
     flex: 1,
     position: 'absolute',
-    top: 0,
+    top: 60,
     right: 0,
     width: Dimensions.get('window').width * 0.9,
+    backgroundColor: theme.grey_light,
+  },
+  scroll: {
+    paddingHorizontal: '8@s',
   },
   btnAdd: {
     padding: 5,
@@ -189,6 +316,7 @@ const styles = ScaledSheet.create({
   },
   row: {
     flexDirection: 'row',
+    alignItems: 'center',
   },
   subTitle: {
     fontSize: '14@ms',
@@ -215,11 +343,36 @@ const styles = ScaledSheet.create({
     borderRadius: 3,
   },
   card: {
-    paddingVertical: '16@vs',
+    borderRadius: 8,
+    paddingHorizontal: '8@s',
+    paddingVertical: '6@vs',
   },
   icon: {
     width: 30,
     height: 30,
+  },
+  btn: {
+    marginTop: '10@vs',
+    marginBottom: '60@vs',
+  },
+  cardItem: {
+    justifyContent: 'space-between',
+  },
+  btnPlus: {
+    paddingHorizontal: '2@s',
+    marginHorizontal: '16@s',
+  },
+  textTitleCar: {
+    fontSize: '14@ms',
+    fontWeight: 'bold',
+  },
+  textSeat: {
+    fontSize: '16@ms',
+    fontWeight: 'bold',
+  },
+  text: {
+    color: theme.white,
+    fontSize: '14@ms',
   },
 });
 
--- src/screens/ListTripPending.js
+++ src/screens/ListTripPending.js
@@ -109,7 +109,7 @@ class ListTripPending extends Component {
             />
           </TouchableOpacity>
         </View>
-        <FilterScreen />
+        {isShowFilter && <FilterScreen hideFilter={this.hideFilter} />}
       </View>
     );
   }
